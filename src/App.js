3878bfbb741d66f7544327b7cb927784341337b7
Samat <fatdrfrog@gmail.com>
2022-11-17 14:55:20 +0600

    removed files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,5 @@
-import { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { Showcase } from "./components/Showcase";
-import { MarvelThemeContext } from "./MarvelThemeContext";
-import { AppContext } from "./Context";
-
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const handleAddtoCart = (item) => {
-    const alreadyinCart = cartItems.find((el) => el.id === item.id);
-
-    if (alreadyinCart) return;
-    setCartItems((prevCarts) => [item, ...prevCarts]);
-  };
-
-  return (
-    <MarvelThemeContext>
-      <AppContext.Provider
-        value={{
-          cartItems,
-          handleAddtoCart,
-        }}
-      >
-        <div
-          className="App"
-          style={{
-            display: "grid",
-            gridTemplateRows: "64px 1fr",
-            width: "100vw",
-            height: "100vh",
-          }}
-        >
-          <Navbar />
-          <Showcase />
-        </div>
-      </AppContext.Provider>
-    </MarvelThemeContext>
-  );
+  return <>shopify</>;
 }
 
 export default App;
